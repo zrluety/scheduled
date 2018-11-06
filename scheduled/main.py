d9d0535b9c85c1543da6e818b6f61091c62c31dc
Zachary Luety <zrluety216@gmail.com>
2018-11-06 08:02:08 -0700

    Save function.

--- scheduled/main.py
+++ scheduled/main.py
@@ -15,7 +15,15 @@ def extract_data(src, profile):
 
     # read pandas args from profile
     pandas_args = profile.get("options").get("pandas")
+    if pandas_args is None:
+        pandas_args = {}
 
     transaction_data = pd.read_excel(src, **pandas_args)
 
     return transaction_data
+
+
+def save(transaction_data, dst):
+    """Save transaction_data to dst."""
+    writer = pd.ExcelWriter(dst)
+    transaction_data.to_excel(writer, "Transactions", index=False, header=False)
